367c908a823e32a98395fd02ac78a497a2ebb118
Lars Moelleken <lars@FUCKUP>
2014-05-29 16:43:00 +0200

    [+]: added more packages from my blog-post -> http://suckup.de/allgemein/toolchain-fuer-webentwickler/

--- .firstInstall.sh
+++ .firstInstall.sh
@@ -2,32 +2,39 @@
 sudo apt-get update
 sudo apt-get upgrade
 
-# sudo apt-get install GNU core utilities
+# default for many other things
+sudo apt-get install build-essential
+
+# install GNU core utilities
 sudo apt-get install coreutils
-# sudo apt-get install some other useful utilities like
+# install some other useful utilities like
 sudo apt-get install moreutils
-# sudo apt-get install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed
+# install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed
 sudo apt-get install findutils
 sudo updatedb
 
 sudo apt-get install bash bash-completion
 
+# trace everything
+sudo apt-get install strace
+
 # more colors
 sudo apt-get install grc
 
-# sudo apt-get install wget / curl
+# install wget / curl
 sudo apt-get install wget curl
 
-# sudo apt-get install node.js
+# install node.js
 # Debian
 curl https://www.npmjs.org/install.sh | sudo sh
-# Ubuntu
-sudo apt-get install nodejs
+# Ubuntu / Debian - sid
+#sudo apt-get install nodejs
 
-# sudo apt-get install more recent versions of some OS X tools
+# install more recent versions of some OS X tools
 sudo apt-get install vim
 
 sudo apt-get install git
+sudo apt-get install ruby-full
 sudo apt-get install imagemagick
 sudo apt-get install lynx
 sudo apt-get install nmap
@@ -37,9 +44,21 @@ sudo apt-get install pv
 sudo apt-get install rename
 sudo apt-get install sqlmap
 sudo apt-get install tree
-sudo apt-get install ucspi-tcp # `tcpserver` et al.
+sudo apt-get install ucspi-tcp
 sudo apt-get install webkit2png
 sudo apt-get install xpdf
 
-# Remove outdated versions from the cellar
+sudo gem install sass
+sudo gem install compass
+sudo gem install autoprefixer-rails
+
+sudo npm install -g bower
+sudo npm install -g grunt-cli
+sudo npm install -g grunt-init
+
+#sudo apt-get install apache2 libapache2-mod-php5 php5-mysql php5-curl php5-gd php5-intl php-pear php5-imagick php5-imap php5-mcrypt php5-memcache php5-ming php5-ps php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl php5-xdebug
+
+# curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/bin
+
+# clean downloaded and already installed packages
 sudo apt-get clean
