7a7b946cd9169305b3e6db957fee722ca704ac3a
Lars Moelleken <lars@FUCKUP>
2014-06-05 00:27:06 +0200

    [+] added composer-install alias

--- .firstInstall.sh
+++ .firstInstall.sh
@@ -108,6 +108,7 @@ sudo aptitude install \
 
 #sudo aptitude install apache2 libapache2-mod-php5 php5-mysql php5-curl php5-gd php5-intl php-pear php5-imagick php5-imap php5-mcrypt php5-memcache php5-ming php5-ps php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl php5-xdebug php5-apcu
 #curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/bin
+#sudo ln -s /usr/bin/composer.phar /usr/bin/composer
 
 # clean downloaded and already installed packages
 sudo aptitude clean
