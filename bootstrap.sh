652a00fd50101664354099fdcda6778e749fb3b2
DrVanScott <newsScott@gmx.de>
2014-06-20 02:14:35 +0200

    gitconfig: notify about local-only changes

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,11 +3,19 @@ cd "$(dirname "${BASH_SOURCE}")"
 git pull origin master
 
 function doIt() {
+  git config --global -l | LANG=C sort > /tmp/oldgit$$
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
         --exclude "README.md" --exclude "firstInstall.sh" \
         --exclude ".gitignore" --exclude ".gitattributes" \
         --exclude "LICENSE-MIT.txt" -avhi --no-perms . ~
 	source ~/.bash_profile
+
+  git config --global -l | LANG=C sort > /tmp/newgit$$
+
+  echo "git configuration not present anymore after bootstrapping:"
+  LANG=C comm -23 /tmp/oldgit$$ /tmp/newgit$$
+  echo -e "\nYou can use the following commands to add it again:"
+  for i in $(LANG=C comm -23 /tmp/oldgit$$ /tmp/newgit$$); do echo "git config --global --add "$(echo $i | sed 's/=/ /') ;done
 }
 
 function dryRun() {
