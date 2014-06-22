e7faf58bf7cea1cbeb557fc33737aba6c702ec7e
Lars Moelleken <lars@moelleken.org>
2014-06-22 16:30:29 +0200

    [+]: added "php5-geoip" for dev

--- firstInstall.sh
+++ firstInstall.sh
@@ -181,7 +181,7 @@ if [[ $? -eq 1 ]]; then
   sudo npm install -g grunt-init
   sudo npm install -g yo
 
-  sudo aptitude install php5-cli php5-mysql php5-curl php5-gd php5-intl php-pear php5-imagick php5-imap php5-mcrypt php5-memcached php5-ming php5-ps php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl php5-xdebug php5-apcu
+  sudo aptitude install php5-cli php5-mysql php5-curl php5-gd php5-intl php-pear php5-imagick php5-imap php5-mcrypt php5-memcached php5-ming php5-ps php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl php5-xdebug php5-apcu php5-geoip
   curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/bin
   sudo ln -s /usr/bin/composer.phar /usr/bin/composer
 fi
