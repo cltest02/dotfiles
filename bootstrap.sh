2f12030129b4da0428f00a74dc3d27e09eed5a0e
Lars Moelleken <voku@voku-online.de>
2014-09-17 03:28:22 +0200

    [+]: "sudo apt-get install zsh"

--- bootstrap.sh
+++ bootstrap.sh
@@ -60,6 +60,7 @@ doIt()
   read -p "Do you want to use the zsh-shell? (y/n) " -n 1 yesOrNo
   echo
   if [[ $yesOrNo =~ ^[Yy]$ ]]; then
+    sudo apt-get install zsh
     chsh -s $(which zsh)
   fi
 
