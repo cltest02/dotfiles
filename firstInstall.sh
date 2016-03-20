e87744bcb312a99651ea271afe13a8f8b4da1412
Lars Moelleken <lars@moelleken.org>
2016-03-20 16:20:20 +0100

    [!]: fixed "firstInstall.sh"

--- firstInstall.sh
+++ firstInstall.sh
@@ -65,7 +65,6 @@ aptitude install \
   advancecomp \
   `# optimize image-size` \
   gifsicle \
-  svgo \
   optipng \
   pngcrush \
   pngnq \
@@ -84,7 +83,7 @@ aptitude install \
   `# removing unneeded localizations` \
   localepurge \
   sysstat \
-  tcpdump
+  tcpdump \
   colordiff \
   moreutils \
   atop \
@@ -148,8 +147,12 @@ aptitude install \
   locales \
   sysstat \
   htop \
-  tcpdump
-  `# nstall python-pygments for json print` \
+  tcpdump \
+  perl \
+  python \
+  python-pip \
+  python-dev \
+  `# install python-pygments for json print` \
   python-pygments
 
 #
@@ -227,6 +230,7 @@ if [[ $? -eq 1 ]]; then
   npm install -g grunt-cli
   npm install -g grunt-init
   npm install -g yo
+  npm install -g svgo
 
   aptitude install php5-cli php5-mysql php5-curl php5-gd php5-intl php-pear php5-imagick php5-imap php5-mcrypt php5-memcached php5-ming php5-ps php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl php5-xdebug php5-apcu php5-geoip
 
