f5b2b74b55f1b5d4bf4993ebaf070029beb6d307
Lars Moelleken <lars@moelleken.org>
2016-05-14 01:41:24 +0200

    [+]: updates / adaptations from "Bash-it/bash-it"

---
+++ .redpill/plugins/plugins/available/history.plugin.bash
@@ -0,0 +1,7 @@
+# enter a few characters and press UpArrow/DownArrow
+# to search backwards/forwards through the history
+if [ -t 1 ]; 
+then
+    bind '"[A":history-search-backward'
+    bind '"[B":history-search-forward'
+fi
