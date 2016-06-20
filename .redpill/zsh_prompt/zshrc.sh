3cbfc4f9eac37caa6918e2d92d88e889024c52dd
Lars Moelleken <lars@moelleken.org>
2016-06-21 00:03:26 +0200

    [+]: update git-zsh-prompt (https://github.com/olivierverdier/zsh-git-prompt/pulls) [+]: added "bin/git-remove-merged-branches.sh"

--- .redpill/zsh_prompt/zshrc.sh
+++ .redpill/zsh_prompt/zshrc.sh
@@ -62,8 +62,7 @@ function update_current_git_vars() {
 
 
 git_super_status() {
-	precmd_update_git_vars
-    if [ -n "$__CURRENT_GIT_STATUS" ]; then
+  if [ -n "$__CURRENT_GIT_STATUS" ]; then
 	  STATUS="$ZSH_THEME_GIT_PROMPT_PREFIX$ZSH_THEME_GIT_PROMPT_BRANCH$GIT_BRANCH%{${reset_color}%}"
 	  if [ "$GIT_BEHIND" -ne "0" ]; then
 		  STATUS="$STATUS$ZSH_THEME_GIT_PROMPT_BEHIND$GIT_BEHIND%{${reset_color}%}"
@@ -82,12 +81,16 @@ git_super_status() {
 		  STATUS="$STATUS$ZSH_THEME_GIT_PROMPT_CHANGED$GIT_CHANGED%{${reset_color}%}"
 	  fi
 	  if [ "$GIT_UNTRACKED" -ne "0" ]; then
-		  STATUS="$STATUS$ZSH_THEME_GIT_PROMPT_UNTRACKED%{${reset_color}%}"
+      STATUS="$STATUS$ZSH_THEME_GIT_PROMPT_UNTRACKED$GIT_UNTRACKED%{${reset_color}%}"
 	  fi
 	  if [ "$GIT_CHANGED" -eq "0" ] && [ "$GIT_CONFLICTS" -eq "0" ] && [ "$GIT_STAGED" -eq "0" ] && [ "$GIT_UNTRACKED" -eq "0" ]; then
 		  STATUS="$STATUS$ZSH_THEME_GIT_PROMPT_CLEAN"
 	  fi
+
+    # remove end space
+    STATUS="$(echo $STATUS | sed 's/ $//g' )"
 	  STATUS="$STATUS%{${reset_color}%}$ZSH_THEME_GIT_PROMPT_SUFFIX"
+
 	  echo "$STATUS"
 	fi
 }
