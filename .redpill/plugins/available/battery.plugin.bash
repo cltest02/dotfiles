5b6896d0c56dec51a471fd2d7d63d603e0374954
lmoelleken <lmoelleken@vdmg-it.com>
2018-01-28 21:37:21 +0100

    ... merge upstream changes v2 - fix (bash)

--- .redpill/plugins/available/battery.plugin.bash
+++ .redpill/plugins/available/battery.plugin.bash
@@ -2,23 +2,23 @@ cite about-plugin
 about-plugin 'display info about your battery charge level'
 
 ac_adapter_connected(){
-  if _command_exists upower;
+  if command_exists upower;
   then
     upower -i $(upower -e | grep BAT) | grep 'state' | grep -q 'charging\|fully-charged'
     return $?
-  elif _command_exists acpi;
+  elif command_exists acpi;
   then
     acpi -a | grep -q "on-line"
     return $?
-  elif _command_exists pmset;
+  elif command_exists pmset;
   then
     pmset -g batt | grep -q 'AC Power'
     return $?
-  elif _command_exists ioreg;
+  elif command_exists ioreg;
   then
     ioreg -n AppleSmartBattery -r | grep -q '"ExternalConnected" = Yes'
     return $?
-  elif _command_exists WMIC;
+  elif command_exists WMIC;
   then
     WMIC Path Win32_Battery Get BatteryStatus /Format:List | grep -q 'BatteryStatus=2'
     return $?
@@ -26,23 +26,23 @@ ac_adapter_connected(){
 }
 
 ac_adapter_disconnected(){
-  if _command_exists upower;
+  if command_exists upower;
   then
     upower -i $(upower -e | grep BAT) | grep 'state' | grep -q 'discharging'
     return $?
-  elif _command_exists acpi;
+  elif command_exists acpi;
   then
     acpi -a | grep -q "off-line"
     return $?
-  elif _command_exists pmset;
+  elif command_exists pmset;
   then
     pmset -g batt | grep -q 'Battery Power'
     return $?
-  elif _command_exists ioreg;
+  elif command_exists ioreg;
   then
     ioreg -n AppleSmartBattery -r | grep -q '"ExternalConnected" = No'
     return $?
-  elif _command_exists WMIC;
+  elif command_exists WMIC;
   then
     WMIC Path Win32_Battery Get BatteryStatus /Format:List | grep -q 'BatteryStatus=1'
     return $?
@@ -55,19 +55,19 @@ battery_percentage(){
 
   declare COMMAND_OUTPUT="no"
 
-  if _command_exists upower;
+  if command_exists upower;
   then
     COMMAND_OUTPUT=$(upower --show-info $(upower --enumerate | grep BAT) | grep percentage | grep -o "[0-9]\+" | head -1)
-  elif _command_exists acpi;
+  elif command_exists acpi;
   then
     COMMAND_OUTPUT=$(acpi -b | awk -F, '/,/{gsub(/ /, "", $0); gsub(/%/,"", $0); print $2}' )
-  elif _command_exists pmset;
+  elif command_exists pmset;
   then
     COMMAND_OUTPUT=$(pmset -g ps | sed -n 's/.*[[:blank:]]+*\(.*%\).*/\1/p' | grep -o "[0-9]\+" | head -1)
-  elif _command_exists ioreg;
+  elif command_exists ioreg;
   then
     COMMAND_OUTPUT=$(ioreg -n AppleSmartBattery -r | awk '$1~/Capacity/{c[$1]=$3} END{OFMT="%05.2f"; max=c["\"MaxCapacity\""]; print (max>0? 100*c["\"CurrentCapacity\""]/max: "?")}' | grep -o "[0-9]\+" | head -1)
-  elif _command_exists WMIC;
+  elif command_exists WMIC;
   then
     COMMAND_OUTPUT=$(WMIC PATH Win32_Battery Get EstimatedChargeRemaining /Format:List | grep -o '[0-9]\+' | head -1)
   else
