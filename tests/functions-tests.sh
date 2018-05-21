e7acc82a81681bbca00b8bc112cbd6822e904353
Lars Moelleken <lars.moelleken@vdmg-it.com>
2018-05-21 11:42:22 +0200

    [+]: add first tests ...
    -> bash + travis-ci (need maybe some debugging)

---
+++ tests/functions-tests.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+. ../.functions
+
+function testLowerCase() {
+  local test_string="ABC_abc_ÖÄÜ_öäü_!\"§";
+  local test_result=$(lc $test_string);
+  local test_expected="abc_abc_öäü_öäü_!\"§";
+
+  assertEquals $test_expected $test_result
+}
+
+function testUpperCase() {
+  local test_string="ABC_abc_ÖÄÜ_öäü_!\"§";
+  local test_result=$(uc $test_string);
+  local test_expected="ABC_ABC_ÖÄÜ_ÖÄÜ_!\"§";
+
+  assertEquals $test_expected $test_result
+}
+
+# run tests
+
+if [ -n "${ZSH_VERSION:-}" ]; then
+  setopt shwordsplit
+
+  SHUNIT_PARENT=$0
+fi
+
+. shunit2
